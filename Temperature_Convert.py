9eebccfe798952f697468f249e91c0a9ca3736b5
GaryXiongXiong <i@jiangyixiong.top>
2019-03-08 21:07:29 +0800

    修正了没写注释没学到东西的问题

--- Temperature_Convert.py
+++ Temperature_Convert.py
@@ -2,7 +2,7 @@
 
 Tempstr = input("请输入温度")
 if Tempstr[-1] in [ 'F' , 'f' ]:
-	C = (eval(Tempstr[0:-1]) - 32)/1.8
+	C = (eval(Tempstr[0:-1]) - 32)/1.8 #通过字符串切片[0:-1]剔除最后一个字符
 	print("输出温度为：{:.2f}C".format(C))
 elif Tempstr[-1] in ['C' , 'c' ]:
 	F = eval(Tempstr[0:-1])*1.8+32
