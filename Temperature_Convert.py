cd30dd36e7b7c21d546513e86f8f146c233f32f6
GaryXiongXiong <i@jiangyixiong.top>
2019-03-08 20:41:42 +0800

    完成温度转换

---
+++ Temperature_Convert.py
@@ -0,0 +1,11 @@
+#Temperature_convert.py
+
+Tempstr = input("请输入温度")
+if Tempstr[-1] in [ 'F' , 'f' ]:
+	C = (eval(Tempstr[0:-1]) - 32)/1.8
+	print("输出温度为：{:.2f}C".format(C))
+elif Tempstr[-1] in ['C' , 'c' ]:
+	F = eval(Tempstr[0:-1])*1.8+32
+	print("输出温度为：{:.2f}F".format(F))
+else:
+	print("输入格式有误，请查证")
